964986ecfdd7c9fc163cecb2cdb9d5677d7bf01c
John Paul Lim Gabule <lucas.gabule@gmail.com>
2020-01-24 03:14:15 +0800

    fetching data with getInitialProps

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,37 @@
-import Link from 'next/link'
 import Layout from "../components/Layout";
+import React, {Component} from 'react';
+import fetch from 'isomorphic-unfetch';
 
-export default () => (
-    <Layout title={"About"}>
-        <p>A JavaScript Developer</p>
-        <img src="/static/javascript-logo-number-angularjs-node-javascript-logo-11563241338go76tq0nxz.png" alt="JavaScript" height="200" />
-    </Layout>
-)
+export default class About extends Component {
+    state = {
+        user: null
+    };
+
+    static async getInitialProps() {
+        const res = await fetch("https://api.github.com/users/reedbarger")
+        const data = await res.json();
+        return {user: data}
+    }
+
+    /*componentDidMount() {
+        fetch("https://api.github.com/users/reedbarger")
+            .then(res => res.json())
+            .then(data => {
+                this.setState({
+                    user: data
+                });
+            });
+    }*/
+
+    render() {
+        const {user} = this.props;
+        return (
+            <Layout title={"About"}>
+                <h2>{user.name}</h2>
+                <p>A JavaScript Developer</p>
+                <img src={user.avatar_url}
+                     alt="Reed" height="200"/>
+            </Layout>
+        )
+    }
+}
