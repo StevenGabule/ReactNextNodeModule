7e5c3b9340c919da9c14c17d9111eb1f97c6f9d0
John Paul Lim Gabule <lucas.gabule@gmail.com>
2020-01-24 02:36:01 +0800

    css with styled jsx

---
+++ pages/about.js
@@ -0,0 +1,9 @@
+import Link from 'next/link'
+import Layout from "../components/Layout";
+
+export default () => (
+    <Layout title={"About"}>
+        <p>A JavaScript Developer</p>
+        <img src="/static/javascript-logo-number-angularjs-node-javascript-logo-11563241338go76tq0nxz.png" alt="JavaScript" height="200" />
+    </Layout>
+)
---
+++ pages/hireme.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Layout from "../components/Layout";
+
+const HireMe = () => (
+    <Layout title={"Hire me"}>
+        <p>You can hire me <a href="mailto:lucasgabule@gmail.com">LucasGabule@gmail.com</a></p>
+    </Layout>
+);
+
+export default HireMe;
